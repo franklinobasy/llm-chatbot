f8a8d2486ca8835b9cf9bb685c1c8eb709a95e36
Franklin Obasi <franklinobasy@gmail.com>
2023-10-04 07:10:54 +0100

    MVP 🚀🚀🚁

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from api import app
 
 
 if __name__ == '__main__':
-    main(model="gpt-3.5-turbo-0301", load_history=True)
+    main(model="gpt-3.5-turbo-0301", load_history=False)
     # sender_email = "tomsguide@smartbrief.com"
     # emails = get_outlook_emails(sender_email)
     # export_sender_email(emails, format='xlsx')
