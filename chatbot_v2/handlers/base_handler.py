256d338e86b9d85b89a3f128ef19f029fa41936d
Franklin Obasi <franklinobasy@gmail.com>
2023-10-20 08:58:16 +0100

    Modified handler error messages

--- chatbot_v2/handlers/base_handler.py
+++ chatbot_v2/handlers/base_handler.py
@@ -8,7 +8,8 @@ class BaseHandler(ABC):
     def __init__(self, section_type: str):
         if section_type not in section_templates.keys():
             raise ValueError(
-                f"This section type: {section_type} is not supported"
+                f"This section type: \"{section_type}\" is not supported.\
+                \nAvailable supported sections are: {section_templates.keys()}"
             )
         self._section_template = section_templates.get(section_type)
 
