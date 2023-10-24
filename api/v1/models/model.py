d8b398b459abd60555c0f8292e67bd0b8f28d3ea
Franklin Obasi <franklinobasy@gmail.com>
2023-10-24 15:33:25 +0100

    Created API service

---
+++ api/v1/models/model.py
@@ -0,0 +1,55 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class Sections(BaseModel):
+    '''
+    Model for sections
+    '''
+    sections: List[str] = []
+
+
+class Questions(BaseModel):
+    '''
+    Model for Ouestions
+    '''
+    section_name: str
+    questions: List[str] = []
+
+
+class Templates(BaseModel):
+    '''
+    Model for Templates
+    '''
+    section_name: str
+    templates = List[str]
+
+
+class UserInput(BaseModel):
+    '''
+    Model for user input
+    '''
+    section_name: str
+    template_index: int
+    answers: List[str] = []
+
+
+class ProposalResult(BaseModel):
+    '''
+    Model for Proposal Generated
+    '''
+    text: str
+
+
+class LetterContext(BaseModel):
+    '''
+    Model for letter context
+    '''
+    context: str
+
+
+class LetterResult(BaseModel):
+    '''
+    Model for Letter generated
+    '''
+    text: str
---
+++ chatbot_v2/templates/context_config.py
@@ -0,0 +1,23 @@
+'''
+Module for configuring LLM messages
+'''
+
+
+ABOUT = '''
+ABOUT CYPHERCRESCENT
+CypherCrescent is a fast-growing wholly indigenous company that provides
+services in energy, technology and human capacity development with core
+expertise in mathematical modelling, petroleum engineering, software
+development and oil & gas asset management consulting. We are committed
+to providing innovative and cost-effective business intelligence solutions
+for E&P companies to increase production, reduce cost and enhance HSE
+across their hydrocarbon value chain.
+'''
+
+LETTER_SYSTEM_PROMPT = f'''
+You are an expert and professional writer who works for cyphercrescent,
+Your job is to use the information provided below to write a very good
+formal letter. You are equied to only outpuf the letter
+
+{ABOUT}
+'''
