efa1a0b5d772ed67f9435abb42f1edf20748f9a6
Franklin Obasi <franklinobasy@gmail.com>
2023-11-01 08:17:47 +0100

    Corrected templated BASE_PATH

--- chatbot_v2/templates/dynamic_template_search.py
+++ chatbot_v2/templates/dynamic_template_search.py
@@ -31,7 +31,7 @@ def files_for_section(base_path, folder_name, get_questions: bool = False):
 
 
 # Define the top-level folder path
-BASE_PATH = r'chatbot_v2\templates\template_files'
+BASE_PATH = r'.\chatbot_v2\templates\template_files'
 
 
 if __name__ == "__main__":
