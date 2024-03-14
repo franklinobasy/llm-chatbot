986a1fdea15afcaf52a19a39f33649356fd46c4a
Franklin Obasi <franklinobasy@gmail.com>
2024-03-14 10:48:18 +0100

    Added Guardrails

--- guardrails/configs/config.py
+++ guardrails/configs/config.py
@@ -4,7 +4,7 @@ import os
 with open(os.path.join(os.getcwd(), "guardrails", "configs", "files", "config.yaml")) as f:
     yaml_content = f.read()
     
-with open(os.path.join(os.getcwd(), "guardrails", "configs", "files", "prompts.yaml")) as f:
+with open(os.path.join(os.getcwd(), "guardrails", "configs", "files", "prompts.co")) as f:
     colang_content = f.read()
     
 config = RailsConfig.from_content(
